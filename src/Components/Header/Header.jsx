bd950ab9c632ca346c689485489a62bcbd496555
moja2890 <foziahussein30@gmail.com>
2025-05-14 12:48:13 -0600

    Banner and Rows

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,39 +1,44 @@
-import React from 'react'
-import "./Header.css"
-import Netflix from "../../assets/Netflix.png"
+import React, { useState } from 'react';
+import './Header.css';
+import Netflix from '../../assets/Netflix.png';
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 
-
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className='header-outer-container'>
-       <div className="header-container">
-        <div className="header-left">
+      <div className='header-container'>
+        <div className='header-left'>
           <ul>
-            <li><img src={Netflix} alt="" /></li>
-            <li>Home</li>
-            <li>Tv Shows</li>
-            <li>Movies</li>
-            <li>Latest</li>
-            <li>My List</li>
-            <li>Browse by Languages</li>
+            <li><img src={Netflix} alt="Netflix Logo" /></li>
+            <div className={`menu-items ${menuOpen ? 'show' : ''}`}>
+              <li>Home</li>
+              <li>Tv Shows</li>
+              <li>Movies</li>
+              <li>Latest</li>
+              <li>My List</li>
+              <li>Browse by Languages</li>
+            </div>
           </ul>
         </div>
 
-        <div className="header-right">
-        <ul>
-          <li><SearchIcon/></li>
-          <li><NotificationsIcon/></li>
-          <li><AccountBoxIcon/></li>
-          <li><ArrowDropDownIcon/></li>
-        </ul>
+        <div className='header-right'>
+          <ul>
+            <li><SearchIcon /></li>
+            <li><NotificationsIcon /></li>
+            <li><AccountBoxIcon /></li>
+            <li onClick={() => setMenuOpen(!menuOpen)}>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
         </div>
-       </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
